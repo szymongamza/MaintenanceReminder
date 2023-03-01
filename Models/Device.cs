97b151a01acaba3efaac2c06ba5b08fab14407ff
Szymon Gamża <gamza.szymon@gmail.com>
2023-03-01 16:14:08 +0100

    Dodaj pliki projektów.

---
+++ Models/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace MaintenanceReminder.Models
+{
+    public class Device
+    {
+        public string Name { get; set; } = null!;
+        public DateTime DateTimeOfLastMaintenance { get; set; }
+        public DateTime DateTimeOfNextMaintenance { get; set; }
+
+        public Device()
+        {
+
+        }
+    }
+
+}
