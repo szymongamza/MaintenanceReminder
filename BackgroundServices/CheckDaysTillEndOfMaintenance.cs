65c291ad10436086914439c00c6af4ca19b8ce2e
Szymon Gamża <gamza.szymon@gmail.com>
2023-03-05 23:09:07 +0100

    quartz

---
+++ BackgroundServices/CheckDaysTillEndOfMaintenance.cs
@@ -0,0 +1,25 @@
+﻿using MaintenanceReminder.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Quartz;
+
+namespace MaintenanceReminder.BackgroundServices;
+
+public class CheckDaysTillEndOfMaintenance : IJob
+{
+    private readonly MaintenanceReminderContext _context;
+
+    public CheckDaysTillEndOfMaintenance(MaintenanceReminderContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var devices = await _context.Device.ToListAsync();
+        foreach (var device in devices)
+        {
+            if(device.DateTimeOfNextMaintenance-DateTime.Now < TimeSpan.FromDays(3.0))
+                Console.WriteLine(device.Name);
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MaintenanceReminder.Data;
+using Quartz;
+using MaintenanceReminder.BackgroundServices;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MaintenanceReminderContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MaintenanceReminderContext") ?? throw new InvalidOperationException("Connection string 'MaintenanceReminderContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddQuartz(q =>
+{
+    q.UseMicrosoftDependencyInjectionJobFactory();
+    var jobKey = new JobKey("CheckDaysTillEndOfMaintenance");
+    q.AddJob<CheckDaysTillEndOfMaintenance>(opts => opts.WithIdentity(jobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(jobKey).WithIdentity("CheckDaysTillEndOfMaintenance-trigger").WithCronSchedule("0 * * ? * *"));
+});
+
+builder.Services.AddQuartzServer(options =>
+{
+    options.WaitForJobsToComplete = true;
+});
 
 var app = builder.Build();
 
